c431df8f1f9e0d9f017f22ecdf326752f99783de
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-01-23 00:21:04 +0530

    created a multiple_constructor in java
    in this file there are many errors and the program is also im=ncomplete to be completed later.

---
+++ many_constructor.java
@@ -0,0 +1,28 @@
+public class Student{
+                string Name;
+                string Course;
+                int roll_number;
+public Student(string name1,  string course1,   int roll_no1)
+      {
+        this.Name = name1;
+        this.Course = course1;
+        this.roll_number = roll_no1;
+        }
+  public Student(string name2, string course2, int roll_no2 )
+        {
+        this.Name = name2;
+        this.Course  = course2;
+        this.roll_number = roll_no2;
+        }
+   public Student(string name3, string course3, int roll_no3)
+        {
+        this.Name = name3;
+        this.Course = course3;
+        this.roll_number = roll_no3;
+        }
+   }
+   public static void main(string[] args)
+   {
+      System.out.println()
+        
+        
