2c128d04dfddb9a531d8db8dbd898aa8c4702e46
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-01-28 15:03:18 +0530

    created my first ArrayList_Example in java.
    In this i have created a free size array in which i have added and removed the list of fruits that i have to buy from the market .

---
+++ ArrayList(ex1).java
@@ -0,0 +1,11 @@
+import java.util.ArrayList; 
+public class Array{
+	public static void main(String[] args){
+ArrayList fruitList = new ArrayList();
+fruitList.add("Mango");
+fruitList.add("apple");
+fruitList.add("strawberry");
+fruitList.remove("strawberry");
+System.out.println(fruitList);
+}
+}
